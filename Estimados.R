34c62febd878583cfc1b6dc6cbda387ab0df27eb
Angel Eduardo Lozano Niño <alozan23@gmail.com>
2021-03-01 11:21:51 -0600

    Version final

--- Estimados.R
+++ Estimados.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(DBI)
 library(RMySQL)
 library(ggplot2)
+library(forecast)
 
 # Se generan series temporales
 
@@ -43,29 +44,29 @@ title(main = "Otros",
 gpu.decom.M = decompose(gpu.ts, type = "mult")
 
 plot(gpu.decom.M, xlab = "Tiempo", 
-     sub = "Descomposición de los datos de uso de AMD")
+     sub = "Descomposici?n de los datos de uso de AMD")
 
 gpu2.decom.M = decompose(gpu2.ts, type = "mult")
 
 plot(gpu.decom.M, xlab = "Tiempo", 
-     sub = "Descomposición de los datos de uso de Intel")
+     sub = "Descomposici?n de los datos de uso de Intel")
 
 gpu3.decom.M = decompose(gpu3.ts, type = "mult")
 
 plot(gpu.decom.M, xlab = "Tiempo", 
-     sub = "Descomposición de los datos de uso de Nvidia")
+     sub = "Descomposici?n de los datos de uso de Nvidia")
 
 gpu4.decom.M = decompose(gpu3.ts, type = "mult")
 
 plot(gpu.decom.M, xlab = "Tiempo", 
-     sub = "Descomposición de los datos de uso de Otras tarjetas")
+     sub = "Descomposici?n de los datos de uso de Otras tarjetas")
 
 #Valor estacional con tendencia
 Trend = gpu.decom.M$trend
 Seasonal = gpu.decom.M$seasonal
 Random = gpu.decom.M$random
 
-ts.plot(cbind(Trend, Trend*Seasonal), xlab = "Tiempo", main = "Datos de uso de AMD", 
+ts.plot1(cbind(Trend, Trend*Seasonal), xlab = "Tiempo", main = "Datos de uso de AMD", 
         ylab = "AMD", lty = 1:2,
         sub = "Tendencia con efectos estacionales multiplicativos sobrepuestos")
 
@@ -79,7 +80,7 @@ Trend2 = gpu2.decom.M$trend
 Seasonal2 = gpu2.decom.M$seasonal
 Random2 = gpu2.decom.M$random
 
-ts.plot(cbind(Trend2, Trend2*Seasonal2), xlab = "Tiempo", main = "Datos de uso de AMD", 
+ts.plot2(cbind(Trend2, Trend2*Seasonal2), xlab = "Tiempo", main = "Datos de uso de AMD", 
         ylab = "Intel", lty = 1:2,
         sub = "Tendencia con efectos estacionales multiplicativos sobrepuestos")
 
@@ -90,10 +91,10 @@ Trend2[100]*Seasonal2[100]*Random2[100]
 gpu2.ts[100]
 
 Trend3 = gpu3.decom.M$trend
-Seasonal = gpu3.decom.M$seasonal
-Random = gpu3.decom.M$random
+Seasonal3 = gpu3.decom.M$seasonal
+Random3 = gpu3.decom.M$random
 
-ts.plot(cbind(Trend3, Trend3*Seasona3l), xlab = "Tiempo", main = "Datos de uso de AMD", 
+ts.plot3(cbind(Trend3, Trend3*Seasonal3), xlab = "Tiempo", main = "Datos de uso de AMD", 
         ylab = "Nvidia", lty = 1:2,
         sub = "Tendencia con efectos estacionales multiplicativos sobrepuestos")
 
@@ -103,9 +104,23 @@ gpu3.ts[7]
 Trend3[100]*Seasonal3[100]*Random3[100]
 gpu3.ts[100]
 
+Trend4 = gpu4.decom.M$trend
+Seasonal4 = gpu4.decom.M$seasonal
+Random4 = gpu4.decom.M$random
+
+ts.plot4(cbind(Trend4, Trend4*Seasonal4), xlab = "Tiempo", main = "Datos de uso de AMD", 
+         ylab = "Nvidia", lty = 1:2,
+         sub = "Tendencia con efectos estacionales multiplicativos sobrepuestos")
+
+Trend4[7]*Seasonal4[7]*Random4[7]
+gpu4.ts[7]
+
+Trend4[100]*Seasonal4[100]*Random4[100]
+gpu4.ts[100]
+
 # 4. Modelo Arima a 12 meses
 
-library(forecast)
+
 arima560 = auto.arima(gpu.ts, stepwise = F,
                       approximation = F,
                       trace = T)
@@ -114,3 +129,33 @@ proy = forecast(arima560,d=1,D=1, h = 12, level = c(12))
 plot(proy)
 autoplot(proy)
 checkresiduals(proy)
+
+
+arima570 = auto.arima(gpu2.ts, stepwise = F,
+                      approximation = F,
+                      trace = T)
+print(arima570)
+proy2 = forecast(arima570,d=1,D=1, h = 12, level = c(12))
+plot(proy2)
+autoplot(proy2)
+checkresiduals(proy2)
+
+
+arima580 = auto.arima(gpu3.ts, stepwise = F,
+                      approximation = F,
+                      trace = T)
+print(arima580)
+proy3 = forecast(arima580,d=1,D=1, h = 12, level = c(12))
+plot(proy3)
+autoplot(proy3)
+checkresiduals(proy3)
+
+
+arima590 = auto.arima(gpu4.ts, stepwise = F,
+                      approximation = F,
+                      trace = T)
+print(arima590)
+proy4 = forecast(arima590,d=1,D=1, h = 12, level = c(12))
+plot(proy4)
+autoplot(proy4)
+checkresiduals(proy4)
